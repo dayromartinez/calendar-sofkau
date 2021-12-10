762c691c5d431f6d5efefdbf9c43fba73fd4a71d
Dayro Martinez <djmd96@hotmail.com>
2021-12-10 17:20:06 -0500

    Conexión MongodB

---
+++ src/main/java/co/com/sofka/calendar/controller/ScheduleController.java
@@ -0,0 +1,30 @@
+package co.com.sofka.calendar.controller;
+
+import co.com.sofka.calendar.model.ProgramDate;
+import co.com.sofka.calendar.services.SchedulerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/api/program")
+public class ScheduleController {
+
+    @Autowired
+    SchedulerService schedulerService;
+
+    @GetMapping("/generate/{date}_{id}")
+    public Flux<ProgramDate> generateCalendar(@PathVariable("date") String date, @PathVariable("id") String id){
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/MM/yyyy");
+
+        //Se convierte el string a LocalDate
+        LocalDate localDate = LocalDate.parse(date, formatter);
+
+        return schedulerService.generateCalendar(id, localDate);
+    }
+}
